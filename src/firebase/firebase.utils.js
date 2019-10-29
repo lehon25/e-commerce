ed1d3206b8a187700737754d859dc38157955688
lehon aritonang <lehon_25@yahoo.com>
2019-10-30 05:02:20 +0700

    sign up component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+
 const config = {
     apiKey: "AIzaSyAl9aqHSDIx6IqvFiVzYiFExitbNveGf9E",
     authDomain: "react-my-burger-49818.firebaseapp.com",
@@ -11,6 +12,26 @@ const config = {
     messagingSenderId: "12302256594", 
     appId: "1:12302256594:web:cbfcf553454ca1dd58e00b"
   }
+
+  export const createUserProfileDocument=async(userAuth,additionalData)=>{
+    if(!userAuth) return
+    const userRef=firestore.doc(`users/${userAuth.uid}`);
+    const snapshot=await userRef.get();
+   if(!snapshot.exists){
+     const {displayName,email}=userAuth;
+     const cretedAt=new Date();
+     try {
+      await userRef.set({
+        displayName,email,cretedAt,...additionalData
+      })
+     }catch(error){
+      console.log('error creating user', error.message)
+     }
+   }
+   return userRef;
+  }
+
+
   firebase.initializeApp(config);
 
   export const auth=firebase.auth();
