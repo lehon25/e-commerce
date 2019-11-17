d6df9d249e61e63f8b190ef98967eafb0af8d2a2
lehon aritonang <lehon_25@yahoo.com>
2019-11-18 02:15:18 +0700

    update application with reselect and cart selector

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch,Route,Redirect } from 'react-router-dom';
 import {connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect'; 
 import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -8,7 +9,7 @@ import Header from'./components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
-
+import {selectCurrentUser} from './redux/user/user.selectors'
 class App extends React.Component {
  
 
@@ -52,11 +53,13 @@ class App extends React.Component {
  
 }
 
-const mapStateToProps=({user})=>({
-  currentUser:user.currentUser
-})
-const mapDispatchToProps=dispatch=>({
-  setCurrentUser:user=>dispatch(setCurrentUser(user))
-})
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
 
 export default connect(mapStateToProps,mapDispatchToProps)(App);
